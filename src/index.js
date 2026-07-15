49e06724267d6a2d28b331d2b21493582206417e
Belli-demo <harold.langenberg@belli.eu>
2026-07-16 00:45:40 +0200

    Delete src/index.js

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppWithOnboarding from './AppWithOnboarding';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><AppWithOnboarding /></React.StrictMode>);
